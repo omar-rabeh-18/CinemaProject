6e2466a0a08acb43f2bb9c8c410db3e64f1e48a9
SaraHossameldin <sarahmfaheem@gmail.com>
2024-03-05 14:04:14 +0200

    The arrays added to the new Users.cpp file

--- Users.cpp
+++ Users.cpp
@@ -10,3 +10,9 @@ extern int ages[100];
 extern int usersCount;
 
 #endif
+#include "Users.h"
+QString usernames[100]={"admin","test", "user1","user2"};
+QString passwords[100]={"admin","1234","1101","1102"};
+int ages[100]={20,21,22,23};
+int usersCount=4;
+
