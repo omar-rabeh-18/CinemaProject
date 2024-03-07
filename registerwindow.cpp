304a743875c1ba2b154809ff5202ae998df4e3a8
SaraHossameldin <sarahmfaheem@gmail.com>
2024-03-07 21:07:24 +0200

    Changess

--- registerwindow.cpp
+++ registerwindow.cpp
@@ -1,6 +1,7 @@
 #include "registerwindow.h"
 #include "ui_registerwindow.h"
 #include <QGroupBox>
+#include<QComboBox>
 #include<Users.h>
 RegisterWindow::RegisterWindow(QWidget *parent)
     : QDialog(parent)
@@ -24,6 +25,7 @@ void RegisterWindow::on_pushButtonRegisterWindow_clicked()
     ui->labelAlreadyExisting->setVisible(false);
     ui->labelNotMatching->setVisible(false);
     ui->labelAgeError->setVisible(false);
+    ui->labelFilledError->setVisible(false);
 
     QString user=ui->labelUsernameRegister->text();
    QString pass1= ui->labelPasswordRegister->text();
@@ -32,10 +34,12 @@ void RegisterWindow::on_pushButtonRegisterWindow_clicked()
   QString mon= ui->comboBoxMonth->currentText();
     int x  =mon.toInt();
 
-    ui-> groupBoxGender->isChecked();
-     ui-> groupBoxBirth->isChecked();
+  ui-> groupBoxGender->isChecked();
+  ui->groupBoxAccountType->isChecked();
+
+  ui-> groupBoxBirth->isChecked();
     ui->groupBoxFavouriteGenre->isChecked();
-     ui->groupBoxAccountType->isChecked();
+
 
     for (int i=0;i<100;i++)
      {
@@ -54,5 +58,15 @@ void RegisterWindow::on_pushButtonRegisterWindow_clicked()
     {
         ui->labelAgeError->setVisible(true);
     }
+     if(user.isEmpty()|| pass1.isEmpty()||pass2.isEmpty()||mon.isEmpty())
+    {
+        ui->labelFilledError->setVisible(true);
+    }
+     if(x==0)
+    {
+       ui->labelFilledError->setVisible(true);
+    }
+
+
 }
 
